f809c09183ae4b139d9307cb4cf9a56d093a33b5
Emmanueltaiwo <77551348+Emmanueltaiwo@users.noreply.github.com>
2022-04-11 15:05:43 -0600

    Update main.js

--- main.js
+++ main.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer", "Graphic Designer", "Social Media Handler", "Freelancer", "Technician"],
+        strings: ["Web Developer","Freelancer",],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer", "Graphic Designer", "Social Media Handler", "Freelancer", "Technician"],
+        strings: ["Web Developer","Freelancer",],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
